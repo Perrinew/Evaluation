4edcc493dc6a882442d9b7f8135a849481ab5102
Perrinew <106061214+Perrinew@users.noreply.github.com>
2022-07-25 10:09:58 +0200

    update2

--- exercices/eventlistener/eventlistener.js
+++ exercices/eventlistener/eventlistener.js
@@ -7,7 +7,7 @@
     prenom = document.getElementById("prenom").value;
 
     document.getElementById("deux").value=(`je m'appelle ${nom} ${prenom}`);
-    document.getElementById("date2").innerHTML=(`DATE: `)
+    document.getElementById("date2").innerHTML=(`ET NOUS SOMMES LE : `)
     document.getElementById("date").innerHTML = Date();
 
 }
