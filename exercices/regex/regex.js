679229a21b1e62e2063029ede1c39ffdd8ce6289
Perrinew <106061214+Perrinew@users.noreply.github.com>
2022-07-26 12:41:21 +0200

    update3

--- exercices/regex/regex.js
+++ exercices/regex/regex.js
@@ -7,6 +7,11 @@ const checkEmail = (value) => {
     const regex2 = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
     return regex2.test(value)
   }
+
+  const checkdatedenaissance = (value) => {
+    const regex3 = /^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/
+    return regex3.test(value)
+  }
   
   const myForm = document.getElementById('myForm')
   let Message
@@ -15,7 +20,7 @@ const checkEmail = (value) => {
     let value = document.getElementById("email").value
     const email = checkEmail(value)
     if (email) {
-      Message = 'Adresse email ajoutée !'
+      Message = 'Adresse e-mail valide !'
     } else {
       Message = 'Veuillez entrer une adresse e-mail valide'
     }
@@ -29,10 +34,23 @@ const checkEmail = (value) => {
     const password = checkpassword(value)
     if (password) {
       Message = 'mdp correcte'
-    } else {
-      Message = 'Veuillez entrer un mdp correcte avec au moins 8 caractères 1 majuscule et 1 caractère spécial et au moins 2 chiffres'
+    } 
+    else {
+      Message = 'Veuillez entrer un mdp correcte avec au moins 8 caractères 1 majuscule et 1 caractère spécial et 1 chiffre'
     }
     document.getElementById("message2").innerHTML = Message
+    e.preventDefault()
+  })
+
+  myForm.addEventListener('submit', function(e) {
+    let value = document.getElementById("datedenaissance").value
+    const datedenaissance= checkdatedenaissance(value)
+    if (datedenaissance) {
+      Message = 'date correcte'
+    } else {
+      Message = 'Veuillez entrer une date correcte'
+    }
+    document.getElementById("message3").innerHTML = Message
     e.preventDefault()
   })
   
